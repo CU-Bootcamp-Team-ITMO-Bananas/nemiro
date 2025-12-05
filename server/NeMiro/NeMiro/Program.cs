4c96fd469759388d3b157c84129e27d82835d8ec
Abbasov Nahid <nabbasov@ozon.ru>
2025-12-06 00:59:17 +0300

    Fix review

--- server/NeMiro/NeMiro/Program.cs
+++ server/NeMiro/NeMiro/Program.cs
@@ -7,18 +7,6 @@ WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 builder.Services
     .AddControllers();
 
-builder.Services.AddCors(options =>
-                         {
-                             options.AddPolicy(
-                                 "AllowFrontend",
-                                 policy =>
-                                 {
-                                     policy.WithOrigins("https://localhost:7066", "http://localhost:5000")
-                                         .AllowAnyMethod()
-                                         .AllowAnyHeader();
-                                 });
-                         });
-
 builder.Services.AddSpaStaticFiles(configuration => { configuration.RootPath = "Client"; });
 
 WebApplication app = builder.Build();
@@ -30,8 +18,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-app.UseStaticFiles();
-app.UseSpaStaticFiles();
 
 app.UseRouting();
 app.UseSwagger();
