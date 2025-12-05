464fcef960e7120cdfe84c11bb5ffc0502c2de39
Mike de Geofroy <mikedegeofroy@gmail.com>
2025-12-06 01:51:44 +0300

    feat: added some interfaces

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,10 @@
+import { Header } from './components/header';
 import { Toolbar } from './components/toolbar';
 
 function App() {
   return (
     <div className='h-fit bg-white'>
+      <Header />
       <Toolbar />
     </div>
   );
