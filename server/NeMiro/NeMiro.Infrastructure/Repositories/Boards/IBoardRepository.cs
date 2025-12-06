1fe8813017ceb84de7a3190aa453144dff822620
Abbasov Nahid <nabbasov@ozon.ru>
2025-12-06 15:31:03 +0300

    Add method

--- server/NeMiro/NeMiro.Infrastructure/Repositories/Boards/IBoardRepository.cs
+++ server/NeMiro/NeMiro.Infrastructure/Repositories/Boards/IBoardRepository.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using NeMiro.Models.Boards;
@@ -8,4 +9,5 @@ public interface IBoardRepository
 {
     Task Create(Board board, CancellationToken cancellationToken);
     Task<Board?> GetById(string id, CancellationToken cancellationToken);
+    Task<IReadOnlyCollection<Board>> GetBoardsByUserId(long id, CancellationToken cancellationToken);
 }
